0fd32ac63ee504174f2bad0cb5893430ccf0ec1c
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-17 16:46:14 +0800

    ✨ feat: 自定义shadow
    EventItem自定义shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 14:36:32
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-17 15:45:13
+ * @FilePath: \next-learn\tailwind.config.js
+ * @Description: tailwindcss 配置文件
+ * 
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved. 
+ */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,6 +22,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        'event-item-shadow': '0 1px 4px rgba(0, 0, 0, 0.3), 0 1px 12px 2px rgba(0, 0, 0, 0.2)'
+      }
     },
   },
   plugins: [],
