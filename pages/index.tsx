51a69c5c15aaf9d1101dae1848888f0a91ac730e
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-17 16:46:31 +0800

    ✨ feat: 渲染列表

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 15:10:42
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-17 16:19:48
+ * @FilePath: \next-learn\pages\index.tsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
+import EventList from '@/components/EventList'
+import { getFeaturedEvents } from '@/data/dummy-data'
 export default function HomePage() {
+    const featuredEvents = getFeaturedEvents()
+
     return (
         <div>
-            <h1>The Home Page</h1>
+            <EventList items={featuredEvents} />
         </div>
     )
 }
