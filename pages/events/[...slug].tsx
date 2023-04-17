40d7182addbd5f6dac367c52daf1fd7dfa046d56
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-17 15:21:59 +0800

    ✨ feat: 筛选的事件页面

---
+++ pages/events/[...slug].tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 15:18:50
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-17 15:19:50
+ * @FilePath: \next-learn\pages\events\[...slug].tsx
+ * @Description: 筛选的事件页面
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
+export default function FilteredEventsPage() {
+    return (
+        <div>
+            <h1>筛选的事件页面</h1>
+        </div>
+    )
+}
