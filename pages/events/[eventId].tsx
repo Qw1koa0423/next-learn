73b968d108cfc216ca18fc4cee6c84504fd722e2
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-17 15:22:17 +0800

    ✨ feat: 事件详细信息页面

---
+++ pages/events/[eventId].tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 15:18:50
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-17 15:19:33
+ * @FilePath: \next-learn\pages\events\[eventId].tsx
+ * @Description: 事件详细信息页面
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
+
+export default function EventDetailPage() {
+    return (
+        <div>
+            <h1>事件详细信息页面</h1>
+        </div>
+    )
+}
