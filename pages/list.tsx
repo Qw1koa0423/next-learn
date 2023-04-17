5725733cfb5e083e0b8c080e0138ff6d31c95994
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-17 14:57:58 +0800

    ✨ feat: 根页面路由

---
+++ pages/list.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 14:56:34
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-17 14:57:21
+ * @FilePath: \next-learn\pages\list.tsx
+ * @Description: 列表页
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
+export default function ListPage() {
+    return (
+        <div>
+            <h1>列表页</h1>
+        </div>
+    )
+}
