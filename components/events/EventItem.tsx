65007aad980d143ac800004f6d91dbd55814d18e
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-17 17:57:58 +0800

    ✨ feat: 使用button

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -2,15 +2,16 @@
  * @Author: 刘浩奇 liuhaoqi@yaozai.net
  * @Date: 2023-04-17 15:22:31
  * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
- * @LastEditTime: 2023-04-17 16:43:58
- * @FilePath: \next-learn\components\EventItem.tsx
+ * @LastEditTime: 2023-04-17 17:52:42
+ * @FilePath: \next-learn\components\events\EventItem.tsx
  * @Description:
  *
  * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
  */
-import Link from 'next/link'
-import React from 'react'
-
+import Button from '../ui/Button'
+import { IconCalendarEvent } from '@tabler/icons-react'
+import { IconMapPin } from '@tabler/icons-react'
+import { IconArrowNarrowRight } from '@tabler/icons-react'
 interface EventItemProps {
     event: {
         id: string
@@ -31,7 +32,7 @@ export default function EventItem(props: EventItemProps) {
     const formatedAddress = location.replace(', ', '\n')
     const exploreLink = `/events/${id}`
     return (
-        <li className="shadow-event-item-shadow rounded-lg overflow-hidden bg-white m-4 flex flex-col gap-4 md:flex-row">
+        <li className="shadow-event-item rounded-lg overflow-hidden bg-white m-4 flex flex-col gap-4 md:flex-row">
             <img
                 src={'/' + image}
                 alt={title}
@@ -39,22 +40,27 @@ export default function EventItem(props: EventItemProps) {
             />
             <div className="w-full px-4 py-0 text-center md:w-3/5 p-0 md:text-left">
                 <div>
-                    <h2 className="my-2 mx-0 md:my-2 md:mx-0 font-bold text-2xl">
-                        {title}
-                    </h2>
+                    <h2 className="my-2 mx-0 md:my-2 md:mx-0">{title}</h2>
                     <div className="flex gap-2 items-center">
+                        <IconCalendarEvent className=" text-[#666] w-5 h-5" />
                         <time className="text-[#666] font-bold">
                             {humanReadableDate}
                         </time>
                     </div>
                     <div className="flex gap-2 items-center">
+                        <IconMapPin className=" text-[#666] w-5 h-5" />
                         <address className=" mx-0 my-2 text-[#666] whitespace-pre">
                             {formatedAddress}
                         </address>
                     </div>
                 </div>
                 <div className=" flex flex-col p-4 md:flex-row md:justify-end">
-                    <Link href={exploreLink}>Explore Event</Link>
+                    <Button link={exploreLink}>
+                        <span>Explore Event</span>
+                        <span className="ml-1  inline-flex justify-center items-center">
+                            <IconArrowNarrowRight className="w-5 h-5" />
+                        </span>
+                    </Button>
                 </div>
             </div>
         </li>
