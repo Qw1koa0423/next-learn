c661ec8d0c35f3b7cf07554a2c33fb580f840967
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-17 14:55:58 +0800

    ✨ feat: 自定义404页面

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 14:55:02
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-17 14:55:23
+ * @FilePath: \next-learn\pages\404.tsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
+export default function NotFoundPage() {
+    return (
+        <div>
+            <h1>找不到该页面！</h1>
+        </div>
+    )
+}
