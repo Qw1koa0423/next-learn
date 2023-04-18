182a942b2454f665ae02a4edc1c232c46e8c3bff
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-18 11:19:39 +0800

    ✨ feat: 使用统一的容器包裹

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 14:36:32
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-18 11:16:51
+ * @FilePath: \next-learn\pages\_app.tsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
+import Layout from '@/components/layout/layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
