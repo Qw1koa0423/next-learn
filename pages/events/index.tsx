53296300c93f20dd2b1e96508a72fca35b92e4bf
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-18 11:24:22 +0800

    ✨ feat: 渲染所有事件列表

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,7 +1,17 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 16:46:37
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-18 11:21:57
+ * @FilePath: \next-learn\pages\events\index.tsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
+import EventList from '@/components/events/EventList'
+import { getAllEvents } from '@/data/dummy-data'
+
 export default function AllEventsPage() {
-    return (
-        <div>
-            <h1>所有事件页面</h1>
-        </div>
-    )
+    const events = getAllEvents()
+    return <EventList items={events} />
 }
