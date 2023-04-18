e329fe670961c2b6a8a63fa4bd43f708aa2e1810
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-18 11:01:22 +0800

    ✨ feat: 使用组件渲染详情

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -2,17 +2,36 @@
  * @Author: 刘浩奇 liuhaoqi@yaozai.net
  * @Date: 2023-04-17 15:18:50
  * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
- * @LastEditTime: 2023-04-17 15:19:33
+ * @LastEditTime: 2023-04-18 09:51:41
  * @FilePath: \next-learn\pages\events\[eventId].tsx
  * @Description: 事件详细信息页面
  *
  * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
  */
-
+import { useRouter } from 'next/router'
+import { getEventById } from '@/data/dummy-data'
+import EventSummary from '@/components/event-detail/EventSummary'
+import EventLogistics from '@/components/event-detail/EventLogistics'
+import EventContent from '@/components/event-detail/EventContent'
 export default function EventDetailPage() {
+    const router = useRouter()
+    const eventId = router.query.eventId
+    const event = getEventById(eventId as string)
+    if (!event) {
+        return <p>暂无结果！</p>
+    }
     return (
-        <div>
-            <h1>事件详细信息页面</h1>
-        </div>
+        <>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </>
     )
 }
