5c2dbbe736335b13ebfcb3f8bee6fe5f51804e6d
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-18 14:08:34 +0800

    ✨ feat: 使用错误提示组件和头部标题组件

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,17 +1,71 @@
+import { getFilteredEvents } from '@/data/dummy-data'
+import { useRouter } from 'next/router'
+import EventList from '@/components/events/EventList'
+import ResultsTitle from '@/components/events/ResultsTitle'
+import Button from '@/components/ui/Button'
+import ErrorAlert from '@/components/ui/ErrorAlert'
 /*
  * @Author: 刘浩奇 liuhaoqi@yaozai.net
  * @Date: 2023-04-17 15:18:50
  * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
- * @LastEditTime: 2023-04-17 15:19:50
+ * @LastEditTime: 2023-04-18 14:01:14
  * @FilePath: \next-learn\pages\events\[...slug].tsx
  * @Description: 筛选的事件页面
  *
  * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
  */
 export default function FilteredEventsPage() {
+    const router = useRouter()
+    const filterData = router.query.slug
+    if (!filterData) {
+        return <p className="center">加载中...</p>
+    }
+    const filteredYear = filterData[0]
+    const filteredMonth = filterData[1]
+    const numYear = +filteredYear
+    const numMonth = +filteredMonth
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return (
+            <>
+                <ErrorAlert>
+                    <p>无效的筛选条件</p>
+                </ErrorAlert>
+                <div className="center">
+                    <Button className=" max-w-max mx-auto" link="/events">
+                        显示所有事件
+                    </Button>
+                </div>
+            </>
+        )
+    }
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    })
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return (
+            <>
+                <ErrorAlert>
+                    <p>没有找到相关的事件</p>
+                </ErrorAlert>
+                <Button className=" max-w-max mx-auto" link="/events">
+                    显示所有事件
+                </Button>
+            </>
+        )
+    }
+    const date = new Date(numYear, numMonth - 1)
     return (
-        <div>
-            <h1>筛选的事件页面</h1>
-        </div>
+        <>
+            <ResultsTitle date={date} />
+            <EventList items={filteredEvents} />
+        </>
     )
 }
--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -2,7 +2,7 @@
  * @Author: 刘浩奇 liuhaoqi@yaozai.net
  * @Date: 2023-04-17 15:18:50
  * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
- * @LastEditTime: 2023-04-18 09:51:41
+ * @LastEditTime: 2023-04-18 14:06:16
  * @FilePath: \next-learn\pages\events\[eventId].tsx
  * @Description: 事件详细信息页面
  *
@@ -13,12 +13,17 @@ import { getEventById } from '@/data/dummy-data'
 import EventSummary from '@/components/event-detail/EventSummary'
 import EventLogistics from '@/components/event-detail/EventLogistics'
 import EventContent from '@/components/event-detail/EventContent'
+import ErrorAlert from '@/components/ui/ErrorAlert'
 export default function EventDetailPage() {
     const router = useRouter()
     const eventId = router.query.eventId
     const event = getEventById(eventId as string)
     if (!event) {
-        return <p>暂无结果！</p>
+        return (
+            <ErrorAlert>
+                <p>暂无结果！</p>
+            </ErrorAlert>
+        )
     }
     return (
         <>
