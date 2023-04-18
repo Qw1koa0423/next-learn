0757f2598ca41f81af3bd89517d34c4a414b297d
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-18 14:40:52 +0800

    ✨ feat: 直接加载静态数据

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
-export default function HomePage() {
+interface HomePageProps {
+    products: {
+        id: string
+        title: string
+    }[]
+}
+
+export default function HomePage(props: HomePageProps) {
+    const { products } = props
     return (
-        <div>
-            <h1>The Home Page</h1>
-        </div>
+        <ul>
+            {products.map((product) => (
+                <li key={product.id}>{product.title}</li>
+            ))}
+        </ul>
     )
+}
+export async function getStaticProps() {
+    return {
+        props: {
+            products: [
+                {
+                    id: 'p1',
+                    title: '结果 1',
+                },
+                {
+                    id: 'p2',
+                    title: '结果 2',
+                },
+                {
+                    id: 'p3',
+                    title: '结果 3',
+                },
+            ],
+        },
+    }
 }
