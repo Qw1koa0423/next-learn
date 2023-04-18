416e118c9a82cfd0f62ea91a5f71e5864027acdd
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-18 11:56:03 +0800

    🐞 fix: 点击事件，样式
    增加button点击事件类型，样式可自定义

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,7 +2,7 @@
  * @Author: 刘浩奇 liuhaoqi@yaozai.net
  * @Date: 2023-04-17 16:49:21
  * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
- * @LastEditTime: 2023-04-18 09:00:53
+ * @LastEditTime: 2023-04-18 11:48:35
  * @FilePath: \next-learn\components\ui\Button.tsx
  * @Description:
  *
@@ -12,17 +12,32 @@ import Link from 'next/link'
 
 interface ButtonProps {
     children: React.ReactNode
-    link: string
+    link?: string
+    onClick?: () => void
+    className?: string
 }
 
 export default function Button(props: ButtonProps) {
-    const { children, link } = props
+    const { children, link, onClick, className } = props
+    if (link) {
+        return (
+            <Link
+                className=" no-underline cursor-pointer inherit-font bg-[#03be9f] rounded-lg border-[1px]  border-solid border-[#03be9f] text-[#dafff7] py-1 px-6 text-center whitespace-nowrap  shadow-button hover:bg-[#02afa1] active:bg-[#02afa1]  flex items-center justify-center"
+                href={link}
+            >
+                {children}
+            </Link>
+        )
+    }
     return (
-        <Link
-            className=" no-underline cursor-pointer inherit-font bg-[#03be9f] rounded-lg border-[1px]  border-solid border-[#03be9f] text-[#dafff7] py-1 px-6 text-center whitespace-nowrap  shadow-button hover:bg-[#02afa1] active:bg-[#02afa1]  flex items-center justify-center"
-            href={link}
+        <button
+            className={
+                'no-underline cursor-pointer inherit-font bg-[#03be9f] rounded-lg border-[1px]  border-solid border-[#03be9f] text-[#dafff7] py-1 px-6 text-center whitespace-nowrap  shadow-button hover:bg-[#02afa1] active:bg-[#02afa1]  flex items-center justify-center ' +
+                className
+            }
+            onClick={onClick}
         >
             {children}
-        </Link>
+        </button>
     )
 }
