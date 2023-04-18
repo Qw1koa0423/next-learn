aaed866902a2e12468626e698c82fa95699df4df
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-18 09:02:29 +0800

    🌈 style: button
    小屏按钮文字居中

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,3 +1,13 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 16:49:21
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-18 09:00:53
+ * @FilePath: \next-learn\components\ui\Button.tsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
 import Link from 'next/link'
 
 interface ButtonProps {
@@ -9,7 +19,7 @@ export default function Button(props: ButtonProps) {
     const { children, link } = props
     return (
         <Link
-            className=" no-underline cursor-pointer inherit-font bg-[#03be9f] rounded-lg border-[1px]  border-solid border-[#03be9f] text-[#dafff7] py-1 px-6 text-center whitespace-nowrap  shadow-button hover:bg-[#02afa1] active:bg-[#02afa1]  flex items-center"
+            className=" no-underline cursor-pointer inherit-font bg-[#03be9f] rounded-lg border-[1px]  border-solid border-[#03be9f] text-[#dafff7] py-1 px-6 text-center whitespace-nowrap  shadow-button hover:bg-[#02afa1] active:bg-[#02afa1]  flex items-center justify-center"
             href={link}
         >
             {children}
