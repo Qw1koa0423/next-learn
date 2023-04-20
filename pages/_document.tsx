7f5ce3f09b24112ba2f22b1b8a9d2d057957797f
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-20 14:21:10 +0800

    ✨ feat: 默认文件，可设置语言和html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,13 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-17 15:07:02
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-20 14:19:11
+ * @FilePath: \next-learn\pages\_document.tsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
+ */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
