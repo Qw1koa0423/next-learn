287600e60a0a9663afe7a49d0714344da50cca91
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-20 13:43:56 +0800

    🐞 fix: 使用useEffect获取详情
    详情不需要被搜索引擎发现

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,34 +1,67 @@
-import { getFilteredEvents } from '@/helpers/api-utils'
 import { useRouter } from 'next/router'
 import EventList from '@/components/events/EventList'
 import ResultsTitle from '@/components/events/ResultsTitle'
 import Button from '@/components/ui/Button'
 import ErrorAlert from '@/components/ui/ErrorAlert'
-import { GetServerSidePropsContext } from 'next'
 import { EnventProps } from '@/types'
+import useSWR from 'swr'
+import { useEffect, useState } from 'react'
 /*
  * @Author: 刘浩奇 liuhaoqi@yaozai.net
  * @Date: 2023-04-17 15:18:50
  * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
- * @LastEditTime: 2023-04-20 13:23:17
+ * @LastEditTime: 2023-04-20 13:38:56
  * @FilePath: \next-learn\pages\events\[...slug].tsx
  * @Description: 筛选的事件页面
  *
  * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
  */
-interface FilteredEventsPageProps {
-    hasError?: boolean
-    events?: EnventProps[]
-    date?: {
-        year: number
-        month: number
+
+export default function FilteredEventsPage() {
+    const router = useRouter()
+    const [loadEvents, setLoadEvents] = useState<EnventProps[]>([])
+    const filterData = router.query.slug as string[]
+    console.log('filterData', filterData)
+
+    const { data, error } = useSWR(
+        'https://nextjs-course-234b1-default-rtdb.firebaseio.com/events.json',
+        (url) => fetch(url).then((res) => res.json())
+    )
+    useEffect(() => {
+        if (data) {
+            const events: EnventProps[] = []
+            for (const key in data) {
+                events.push({
+                    id: key,
+                    ...data[key],
+                })
+            }
+            setLoadEvents(events)
+        }
+    }, [data])
+    if (!loadEvents) {
+        return <p className="center">加载中...</p>
     }
-}
-
-export default function FilteredEventsPage(props: FilteredEventsPageProps) {
-    const { hasError, events, date } = props
-
-    if (hasError) {
+    const filteredYear = filterData[0]
+    const filteredMonth = filterData[1]
+    const numYear = +filteredYear
+    const numMonth = +filteredMonth
+    const filteredEvents = loadEvents.filter((event) => {
+        const eventDate = new Date(event.date)
+        return (
+            eventDate.getFullYear() === numYear &&
+            eventDate.getMonth() === numMonth - 1
+        )
+    })
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12 ||
+        error
+    ) {
         return (
             <>
                 <ErrorAlert>
@@ -42,7 +75,6 @@ export default function FilteredEventsPage(props: FilteredEventsPageProps) {
             </>
         )
     }
-    const filteredEvents = events
     if (!filteredEvents || filteredEvents.length === 0) {
         return (
             <>
@@ -55,49 +87,11 @@ export default function FilteredEventsPage(props: FilteredEventsPageProps) {
             </>
         )
     }
-    const dateObj = new Date(date!.year, date!.month - 1)
+    const date = new Date(numYear, numMonth - 1)
     return (
         <>
-            <ResultsTitle date={dateObj} />
+            <ResultsTitle date={date} />
             <EventList items={filteredEvents} />
         </>
     )
-}
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-    const { params } = context
-    const filterData = params?.slug as string[]
-
-    const filteredYear = filterData[0]
-    const filteredMonth = filterData[1]
-    const numYear = +filteredYear
-    const numMonth = +filteredMonth
-    if (
-        isNaN(numYear) ||
-        isNaN(numMonth) ||
-        numYear > 2030 ||
-        numYear < 2021 ||
-        numMonth < 1 ||
-        numMonth > 12
-    ) {
-        return {
-            props: {
-                hasError: true,
-            },
-            /** 返回404 */
-            // notFound: true,
-        }
-    }
-    const filteredEvents = await getFilteredEvents({
-        year: numYear,
-        month: numMonth,
-    })
-    return {
-        props: {
-            events: filteredEvents,
-            date: {
-                year: numYear,
-                month: numMonth,
-            },
-        },
-    }
 }
