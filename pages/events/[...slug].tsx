cb6611abe197c0bcd02776a1e9b4e0dc81663edd
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-20 13:28:28 +0800

    ✨ feat: 服务端渲染

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,37 +1,34 @@
-import { getFilteredEvents } from '@/data/dummy-data'
+import { getFilteredEvents } from '@/helpers/api-utils'
 import { useRouter } from 'next/router'
 import EventList from '@/components/events/EventList'
 import ResultsTitle from '@/components/events/ResultsTitle'
 import Button from '@/components/ui/Button'
 import ErrorAlert from '@/components/ui/ErrorAlert'
+import { GetServerSidePropsContext } from 'next'
+import { EnventProps } from '@/types'
 /*
  * @Author: 刘浩奇 liuhaoqi@yaozai.net
  * @Date: 2023-04-17 15:18:50
  * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
- * @LastEditTime: 2023-04-18 14:01:14
+ * @LastEditTime: 2023-04-20 13:23:17
  * @FilePath: \next-learn\pages\events\[...slug].tsx
  * @Description: 筛选的事件页面
  *
  * Copyright (c) 2023 by 遥在科技, All Rights Reserved.
  */
-export default function FilteredEventsPage() {
-    const router = useRouter()
-    const filterData = router.query.slug
-    if (!filterData) {
-        return <p className="center">加载中...</p>
+interface FilteredEventsPageProps {
+    hasError?: boolean
+    events?: EnventProps[]
+    date?: {
+        year: number
+        month: number
     }
-    const filteredYear = filterData[0]
-    const filteredMonth = filterData[1]
-    const numYear = +filteredYear
-    const numMonth = +filteredMonth
-    if (
-        isNaN(numYear) ||
-        isNaN(numMonth) ||
-        numYear > 2030 ||
-        numYear < 2021 ||
-        numMonth < 1 ||
-        numMonth > 12
-    ) {
+}
+
+export default function FilteredEventsPage(props: FilteredEventsPageProps) {
+    const { hasError, events, date } = props
+
+    if (hasError) {
         return (
             <>
                 <ErrorAlert>
@@ -45,10 +42,7 @@ export default function FilteredEventsPage() {
             </>
         )
     }
-    const filteredEvents = getFilteredEvents({
-        year: numYear,
-        month: numMonth,
-    })
+    const filteredEvents = events
     if (!filteredEvents || filteredEvents.length === 0) {
         return (
             <>
@@ -61,11 +55,49 @@ export default function FilteredEventsPage() {
             </>
         )
     }
-    const date = new Date(numYear, numMonth - 1)
+    const dateObj = new Date(date!.year, date!.month - 1)
     return (
         <>
-            <ResultsTitle date={date} />
+            <ResultsTitle date={dateObj} />
             <EventList items={filteredEvents} />
         </>
     )
+}
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const { params } = context
+    const filterData = params?.slug as string[]
+
+    const filteredYear = filterData[0]
+    const filteredMonth = filterData[1]
+    const numYear = +filteredYear
+    const numMonth = +filteredMonth
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return {
+            props: {
+                hasError: true,
+            },
+            /** 返回404 */
+            // notFound: true,
+        }
+    }
+    const filteredEvents = await getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    })
+    return {
+        props: {
+            events: filteredEvents,
+            date: {
+                year: numYear,
+                month: numMonth,
+            },
+        },
+    }
 }
