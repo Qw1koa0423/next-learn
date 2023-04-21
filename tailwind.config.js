550217457c54ec04051deb0e084a7c737e1736b4
刘浩奇 <liuhaoqi@yaozai.net>
2023-04-21 17:51:45 +0800

    ✨ feat: 清除tailwindcss覆盖的默认样式

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: 刘浩奇 liuhaoqi@yaozai.net
+ * @Date: 2023-04-21 17:04:17
+ * @LastEditors: 刘浩奇 liuhaoqi@yaozai.net
+ * @LastEditTime: 2023-04-21 17:49:20
+ * @FilePath: \next-learn\tailwind.config.js
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by 遥在科技, All Rights Reserved. 
+ */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -15,4 +25,7 @@ module.exports = {
     },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
 }
